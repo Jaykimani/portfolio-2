036f5fbed4b35183fee65e328dd5d95fa25fb852
Jaykimani <josephkimani1998@gmail.com>
2021-11-29 12:31:23 +0300

    introducing threejs

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-const express = require ("express");
+const express = require("express");
+var path = require('path')
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, '/public')))
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
